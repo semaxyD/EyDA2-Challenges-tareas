fd6d2fec4be7869a5c845f837b9bdcd0ee3f09dd
semaxyD <sebucho48@gmail.com>
2025-02-18 14:42:33 -0500

    Challenge2

---
+++ Challenges/challenges/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FirstApp from "./Challenge2/FirstApp";
+
+const App = () => {
+  return (
+    <div>
+      <FirstApp />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ Challenges/challenges/src/Challenge2/FirstApp.jsx
@@ -0,0 +1,10 @@
+const FirstApp = () => {
+    return (
+        <div>
+          <h1>My First React Component</h1>
+          <span>10</span>
+        </div>
+      );
+};
+
+export default FirstApp 
